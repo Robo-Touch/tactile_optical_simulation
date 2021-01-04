b45f46cd41576dc1d1e4a131e90ba35bef6da143
arpit agarwal <arpit15945@gmail.com>
2021-01-04 10:23:51 -0500

    create intermediate dirs

--- tactile_optical_simulation/folder_utils.py
+++ tactile_optical_simulation/folder_utils.py
@@ -3,7 +3,7 @@ from os import path as osp
 
 def create_folder(project_name, overwrite = False):
   try:
-    os.mkdir(osp.join('results', project_name))
+    os.makedirs(osp.join('results', project_name))
   except FileExistsError:
     if(not overwrite):
       tmp = input("Dir exists. Do you want to overwrite(Y/n)?")
