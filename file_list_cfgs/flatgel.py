9f973a76c0a3d24465b690dcc98f596dea1ea5ae
arpit15 <arpit15945@gmail.com>
2023-07-27 22:22:17 -0700

    change target file to png

--- file_list_cfgs/flatgel.py
+++ file_list_cfgs/flatgel.py
@@ -7,7 +7,7 @@ data_folder = join("target_settings", "flatgel")
 img_folder = join(data_folder, "imgs")
 
 img_list = [
-	join(img_folder, "tri1u.exr")
+	join(img_folder, "pngs", "tri1u.png")
 ]
 
 
